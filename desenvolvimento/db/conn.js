f364fd553722ba480b9ba6d6726d58ba66fa09e1
Nicolas Figueiredo <nicolasfigueiredo@gmail.com>
2024-09-05 15:48:17 -0300

    teste deploy

--- desenvolvimento/db/conn.js
+++ desenvolvimento/db/conn.js
@@ -1,45 +1,30 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
-// Acessa as variáveis de ambiente
-const dbName = process.env.DB_DBNAME;
-const dbUser = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-    host: dbHost,
-    port: dbPort, // Porta do banco de dados
-    dialect: 'mysql',
-    define: {
+// Configuração do Sequelize
+const sequelize = new Sequelize(
+    process.env.DB_DBNAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST,
+        port: process.env.PORT,
+        dialect: 'mysql',
+        define: {
         freezeTableName: true, // Desativa a pluralização automática
     },
-    logging: console.log,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000, // Tempo limite para adquirir uma conexão
-        idle: 10000 // Tempo limite para conexão ociosa
-    },
-    dialectOptions: {
-        connectTimeout: 60000 // Tempo limite de conexão
-    }
 });
 
-/*const sequelize = new Sequelize('admschool','admin','admin1234',{
-    host: 'admschool.cj1hbdrfyshz.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql',
-    define: {
-        freezeTableName: true, // Desativa a pluralização automática
-    },
-})*/
-
-try {
-    sequelize.authenticate()
-    console.log('Conectamos com sucesso!')
-} catch(err){
-    console.log(`Não foi possível conectar: ${err}`)
+// Função para verificar a conexão com o banco de dados
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conectamos com sucesso!');
+    } catch (err) {
+        console.error(`Não foi possível conectar: ${err}`);
+    }
 }
 
-module.exports = sequelize
+
+module.exports = sequelize;
